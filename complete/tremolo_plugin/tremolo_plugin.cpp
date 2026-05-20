47340fb92fdb4a8d2d6c75fcaa59763e9d066ef8
tylernsnow <tnsnow23@gmail.com>
2026-05-19 23:55:10 -0500

    Removed "complete" folder
    Removed "complete" folder from side plugin

--- complete/tremolo_plugin/tremolo_plugin.cpp
+++
@@ -1,8 +0,0 @@
-#include "tremolo_plugin.h"
-#include <TremoloPluginAssets.h>
-#include "source/LfoVisualizer.cpp"
-#include "source/CustomLookAndFeel.cpp"
-#include "source/JsonSerializer.cpp"
-#include "source/Parameters.cpp"
-#include "source/PluginEditor.cpp"
-#include "source/PluginProcessor.cpp"
